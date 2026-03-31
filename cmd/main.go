4d2e1594040ed33938c4b3f02f5995e959e0100c
starslan <starslan-1@yandex>
2026-03-31 11:22:27 +0300

    CLI

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"go.uber.org/zap"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+
+	logger.Info("Application started")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		if !scanner.Scan() {
+			break
+		}
+		question := scanner.Text()
+
+		answer := fmt.Sprintf("You asked: %s", question)
+		logger.Info("Response", zap.String("answer", answer))
+		fmt.Println("Response:", answer)
+	}
+
+	if err := scanner.Err(); err != nil {
+		logger.Error("Error I/O scanner", zap.Error(err))
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module storage
 
 go 1.25
+
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.1 // indirect
+)
