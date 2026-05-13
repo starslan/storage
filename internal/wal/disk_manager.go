71acb0f942ab71a5f76910a2d2db8995b2e624a8
starslan <starslan-1@yandex>
2026-05-13 12:18:53 +0300

    split code by files

--- internal/wal/disk_manager.go
+++ internal/wal/disk_manager.go
@@ -2,16 +2,11 @@ package wal
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
-	"path/filepath"
 	"regexp"
-	"sort"
 	"storage/internal/config"
 	"storage/pkg/utils"
-	"strconv"
-	"strings"
 	"sync"
 
 	"go.uber.org/zap"
@@ -74,34 +69,6 @@ func (dm *DiskManager) Flush(records []record) error {
 	return nil
 }
 
-func (dm *DiskManager) Load(action func(string) error) (*int, error) {
-	names, err := filepath.Glob(dm.DataPath + "/*.log")
-	if err != nil {
-		return nil, err
-	}
-
-	if len(names) == 0 {
-		return nil, dm.rotateLogFile()
-	}
-
-	sort.Strings(names)
-
-	var lastID *int
-	for i, name := range names {
-		last := i == len(names)-1
-		var id *int
-		if id, err = dm.loadFromFile(name, action, last); err != nil {
-			return nil, err
-		}
-		if last && id != nil {
-			lastID = id
-		}
-	}
-
-	return lastID, nil
-
-}
-
 func (dm *DiskManager) rotateLogFile() error {
 	dm.mutex.Lock()
 	defer dm.mutex.Unlock()
@@ -116,89 +83,5 @@ func (dm *DiskManager) rotateLogFile() error {
 		return err
 	}
 	dm.currentFile = file
-	return nil
-}
-
-func (dm *DiskManager) loadFromFile(fileName string, action func(string) error, last bool) (*int, error) {
-	file, err := os.Open(fileName)
-	defer func() {
-		if !last {
-			_ = file.Close()
-		}
-	}()
-	if err != nil {
-		return nil, err
-	}
-
-	scanner := bufio.NewScanner(file)
-	ids := make([]int, 0, dm.batchSize)
-	dataMap := map[int]string{}
-	var lastId int
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "" {
-			continue
-		}
-		bf, af, f := strings.Cut(line, "_")
-		if !f {
-			return nil, errors.New("record not parse")
-		}
-		id, err := strconv.Atoi(bf)
-		if err != nil {
-			return nil, err
-		}
-		ids = append(ids, id)
-		dataMap[id] = af
-	}
-
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-
-	if last {
-		_ = file.Close()
-
-		file, err = os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0644)
-		if err != nil {
-			return nil, err
-		}
-
-		dm.currentFile = file
-		numStr := strings.TrimSuffix(filepath.Base(file.Name()), ".log")
-		number, err := strconv.Atoi(numStr)
-		if err != nil {
-			return nil, err
-		}
-		dm.fileNameCount = number
-	}
-
-	if len(ids) == 0 {
-		return nil, nil
-	}
-
-	sort.Ints(ids)
-
-	if last {
-		lastId = ids[len(ids)-1]
-	}
-
-	for _, id := range ids {
-		if err := action(dataMap[id]); err != nil {
-			return nil, err
-		}
-	}
-	return &lastId, nil
-}
-
-func (dm *DiskManager) setCurrentFile(file *os.File) error {
-	dm.currentFile = file
-	numStr := strings.TrimSuffix(filepath.Base(file.Name()), ".log")
-
-	number, err := strconv.Atoi(numStr)
-	if err != nil {
-		return err
-	}
-	dm.fileNameCount = number
 	return nil
 }
---
+++ internal/wal/load_data.go
@@ -0,0 +1,111 @@
+package wal
+
+import (
+	"bufio"
+	"errors"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func (dm *DiskManager) Load(action func(string) error) (*int, error) {
+	names, err := filepath.Glob(dm.DataPath + "/*.log")
+	if err != nil {
+		return nil, err
+	}
+
+	if len(names) == 0 {
+		return nil, dm.rotateLogFile()
+	}
+
+	sort.Strings(names)
+
+	var lastID *int
+	for i, name := range names {
+		last := i == len(names)-1
+		var id *int
+		if id, err = dm.loadFromFile(name, action, last); err != nil {
+			return nil, err
+		}
+		if last && id != nil {
+			lastID = id
+		}
+	}
+
+	return lastID, nil
+
+}
+
+func (dm *DiskManager) loadFromFile(fileName string, action func(string) error, last bool) (*int, error) {
+	file, err := os.Open(fileName)
+	defer func() {
+		if !last {
+			_ = file.Close()
+		}
+	}()
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(file)
+	ids := make([]int, 0, dm.batchSize)
+	dataMap := map[int]string{}
+	var lastId int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		bf, af, f := strings.Cut(line, "_")
+		if !f {
+			return nil, errors.New("record not parse")
+		}
+		id, err := strconv.Atoi(bf)
+		if err != nil {
+			return nil, err
+		}
+		ids = append(ids, id)
+		dataMap[id] = af
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	if last {
+		_ = file.Close()
+
+		file, err = os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			return nil, err
+		}
+
+		dm.currentFile = file
+		numStr := strings.TrimSuffix(filepath.Base(file.Name()), ".log")
+		number, err := strconv.Atoi(numStr)
+		if err != nil {
+			return nil, err
+		}
+		dm.fileNameCount = number
+	}
+
+	if len(ids) == 0 {
+		return nil, nil
+	}
+
+	sort.Ints(ids)
+
+	if last {
+		lastId = ids[len(ids)-1]
+	}
+
+	for _, id := range ids {
+		if err := action(dataMap[id]); err != nil {
+			return nil, err
+		}
+	}
+	return &lastId, nil
+}
